569d4a8f3a78702529161471fb07e23d64f828ac
Mayank Gupta <142477055+Mugta1@users.noreply.github.com>
2024-06-28 13:29:03 +0000

    Half-done

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 import time
+time=time.time()
 while True:
-    time=time.time()
-    print(time)
+    time2=time.time()
+    for time-time2==
 
 tempfiledirectory=["C:\Windows\Temp", "C:\Users\mg15d\AppData\Local\Temp"]
+
+for directory in tempfiledirectory:
+    if os.path.isdir(directory):
+        for path, subdir, files in os.walk(direcotry):
+            try:
+                os.remove(file in files)
+            except:
+                continue
