87292fe35af92735e5859279bb27964b11894648
Mayank Gupta <142477055+Mugta1@users.noreply.github.com>
2024-06-28 13:59:14 +0000

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import time
-time=time.time()
 def deleter():
     tempfiledirectory=[r"C:\Windows\Temp", r"C:\Users\mg15d\AppData\Local\Temp" ]
 
@@ -9,15 +8,18 @@ def deleter():
             for path, subdir, files in os.walk(hehe):
                 for file in files:
                     x=os.path.join(path, file)
-                try:
-                    os.remove(x)
-                except:
-                    continue
-while True:
-    time2=time.time()
-    x=time-time2
-    if x >=172800:
-        choice=input("It's been two days since the temperorary files were last deleted, Delete files again? Yes/No ")
-        if choice.lower()=="yes":
-            deleter()
+                    try:
+                        os.remove(x)
+                    except:
+                        print(f"{file} not deleted")
 
+def main():
+    time=time.sleep(172800)
+
+    choice=input("It's been two days since the temperorary files were last deleted, Delete files again? Yes/No ")
+    if choice.lower()=="yes":
+        deleter()
+        
+    
+
+main()     
